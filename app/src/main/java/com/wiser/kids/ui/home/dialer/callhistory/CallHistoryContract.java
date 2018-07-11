678f07ce4f9b10d006601aaa44a34ae374ddaedc
abidhussnain-algo <38759685+abidhussnain-algo@users.noreply.github.com>
2018-07-11 19:00:09 +0500

    task complete

---
+++ app/src/main/java/com/wiser/kids/ui/home/dialer/callhistory/CallHistoryContract.java
@@ -0,0 +1,18 @@
+package com.wiser.kids.ui.home.dialer.callhistory;
+
+import com.wiser.kids.BasePresenter;
+import com.wiser.kids.BaseView;
+
+public class CallHistoryContract {
+
+    interface View extends BaseView<CallHistoryContract.Presenter>
+    {
+
+    }
+
+    interface Presenter extends BasePresenter
+    {
+
+    }
+
+}
